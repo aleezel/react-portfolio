72a4c9a9add32730d04f6e74b2c087d36316ae47
Ale <alejandra.piedra02@gmail.com>
2024-02-01 22:50:18 -0600

    Configuración de dependencias, archivos y algunos estilos listos para empezar la aventura!

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const App = () =>(
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1>Hellou</h1>
+    </div>
+  );
+
+  export default App
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6278EF",
+        secondary: '#323070',
+        txt: "#4A5385",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        roboto: ["Roboto Flex", "sans-serif"],
+        switzer: ["Switzer", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
+
